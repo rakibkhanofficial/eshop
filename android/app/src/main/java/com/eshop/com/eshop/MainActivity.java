b4603e96917e12fc488421e632299bb34fce758b
rakibkhanofficial <khanrakib218@gmail.com>
2023-08-16 22:47:01 +0600

    Initial commit

---
+++ android/app/src/main/java/com/eshop/com/eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.eshop.com.eshop;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+ import 'src/menu.dart';
+import 'src/navigation_controls.dart';
+import 'src/web_view_stack.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://shop.akijvg.net/'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Eshop'),
+        actions: [
+          NavigationControls(controller: controller),
+           Menu(controller: controller),
+        ],
+      ),
+       body: WebViewStack(controller: controller),
+    );
+  }
+}
